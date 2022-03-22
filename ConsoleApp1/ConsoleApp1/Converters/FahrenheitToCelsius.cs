5911e047af2c6235977e816168a02b525d363dfc
Sina <pournaeim@gmail.com>
2022-03-23 00:29:43 +0430

    Change dFahrenheitToCelsius.cs

--- ConsoleApp1/ConsoleApp1/Converters/FahrenheitToCelsius.cs
+++ ConsoleApp1/ConsoleApp1/Converters/FahrenheitToCelsius.cs
@@ -8,9 +8,14 @@ namespace ConsoleApp1.Converters
     {
         public double FahrenheitToCelsiusConverter(double FahrenheitTemp)
         {
-            double _fahreneithTemp = 0.0;
             double _celsius = 0.0;
-            return _celsius;
+// (F – 32) * 5/9
+var step1 = Calculator.Calculator.Subtract(
+fahrenheitTemp, 32);
+var step2 = Calculator.Calculator.Multiplication(
+step1, 5.0);
+_celsius = Calculator.Calculator.Division(step2, 9.0);
+return _celsius;
         }
     }
 }
