59a684fff1543fd363cff862d1ba850240b9685a
Sina <pournaeim@gmail.com>
2022-03-22 21:23:06 +0430

    Create skeleton for FtoC converter

---
+++ ConsoleApp1/ConsoleApp1/Converters/FahrenheitToCelsius.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Converters
+{
+    public class FahrenheitToCelsius
+    {
+        public double FahrenheitToCelsiusConverter(double FahrenheitTemp)
+        {
+            double _fahreneithTemp = 0.0;
+            double _celsius = 0.0;
+            return _celsius;
+        }
+    }
+}
