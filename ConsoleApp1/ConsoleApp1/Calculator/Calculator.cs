c7d4e1579f0fe75cb3210754639afb0ae97ce714
Sina <pournaeim@gmail.com>
2022-03-22 21:38:38 +0430

    Create Calculator class and method Add

---
+++ ConsoleApp1/ConsoleApp1/Calculator/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Calculator
+{
+    public class Calculator
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
